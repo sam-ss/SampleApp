b9108475ffeb66e8f243208d83a637ba853c5949
Sumeet <ssaraf@xpanxion.com>
2019-01-23 19:37:27 +0530

    MAP related files added

--- SampleApp/SampleApp/App.xaml.cs
+++ SampleApp/SampleApp/App.xaml.cs
@@ -4,6 +4,7 @@ using SampleApp.Business.DataBase;
 using SampleApp.Business.Interfaces;
 using SampleApp.ViewModels;
 using SampleApp.Views;
+using SampleApp.Views.MapPages;
 using SampleApp.Views.XamlControls;
 using SQLite;
 using System;
@@ -27,9 +28,33 @@ namespace SampleApp
 
             // MainPage = new XamlControlPage();
             //MainPage = new NavigationPage(new EmployeeListPage());
-            SimpleIoc.Default.Register(() => UserDialogs.Instance);
-            App.Current.MainPage = new NavigationPage(new Views.LoginPage());
-            
+
+            //SimpleIoc.Default.Register(() => UserDialogs.Instance);
+            //App.Current.MainPage = new NavigationPage(new Views.LoginPage());
+
+            try
+            {
+                var tabs = new TabbedPage();
+
+                // demonstrates the map control with zooming and map-types
+                tabs.Children.Add(new MapPage { Title = "Map/Zoom", Icon = "glyphish_74_location.png" });
+
+                // demonstrates the map control with zooming and map-types
+                tabs.Children.Add(new PinPage { Title = "Pins", Icon = "glyphish_07_map_marker.png" });
+
+                // demonstrates the Geocoder class
+                tabs.Children.Add(new GeocoderPage { Title = "Geocode", Icon = "glyphish_13_target.png" });
+
+                // opens the platform's native Map app
+                tabs.Children.Add(new MapAppPage { Title = "Map App", Icon = "glyphish_103_map.png" });
+
+                MainPage = tabs;
+            }
+            catch (Exception ex)
+            {
+
+                 
+            }
         }
 
         public static DatabaseService Database
